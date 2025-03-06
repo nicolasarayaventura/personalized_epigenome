4326ec46608b19bed01022cff284889beda5528b
Nicolas Araya Ventura <arayan01@li04e02.chimera.hpc.mssm.edu>
2025-03-06 12:39:23 -0500

    upd.

--- results/2025-02-24_atachg38/run.sh
+++ results/2025-02-24_atachg38/run.sh
@@ -1,28 +1,27 @@
 set -x -e
 
 adapter_path="/sc/arion/work/arayan01/test-env/envs/atacseq/share/trimmomatic-0.39-2/adapters/NexteraPE-PE.fa"
-scratch="/sc/arion/scratch/arayan01/projects/personalized_epigenome/data/2025-02-24_atac"
+scratch="/sc/arion/scratch/arayan01/projects/personalized_epigenome/data/2025-02-24_atachg38"
 work="/sc/arion/work/arayan01/project/personalized_epigenome/results/2025-02-24_atachg38"
 
 function samplelist {
 	rm -rf "${work}/sample_ids.txt"
-	sampledir="${scratch}/2025-02-19_ranjan_data/2022-11-22_data_from_Ranjan_Xiang/atac"
+	sampledir="/sc/arion/scratch/arayan01/projects/personalized_epigenome/data/2025-02-19_ranjan_data/2022-11-22_data_from_Ranjan_Xiang/atac"
 	output="${work}/sample_ids.txt"
 	
 	samplepath=$(realpath "${sampledir}")
     find "${sampledir}" -name "*_R1_001.fastq.gz" | while read R1; do
         sample=$(basename "${R1}" | sed 's/_R1_001.fastq.gz//')
         R2="${R1/_R1_001.fastq.gz/_R2_001.fastq.gz}"
-        echo -e "${sample}\t${R1}.fasta.gz\t${R2}.fasta.gz" >> "${output}"
+        echo -e "${sample}\t${R1}\t${R2}" >> "${output}"
     done
 
 }
 
 function fastqc_initial {
 	samplelist="${work}/sample_ids.txt"
-	sampledir="${scratch}/2025-02-19_ranjan_data/2022-11-22_data_from_Ranjan_Xiang/atac"
-	qc_out="${work}/qc/init_fastqc" #once job is finished run multiqc in directory!
-
+    qc_out="${work}/qc/init_fastqc" #once job is finished run multiqc in directory!
+	sampledir="/sc/arion/scratch/arayan01/projects/personalized_epigenome/data/2025-02-19_ranjan_data/2022-11-22_data_from_Ranjan_Xiang/atac"
 	while IFS=$'\t' read -r sample fastq_file; do
 		bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${scratch}/job_atacfastqc_${sample}.txt" \
 		fastqc -t 2 "${sampledir}/${fastq_file}" -o "${qc_out}"
@@ -30,8 +29,8 @@ function fastqc_initial {
 }
 
 function trimming {
-	rm -rf "${scratch}/trimmed"
-	mkdir -p "${scratch}/trimmed"
+	rm -rf "${scratch}/trimming"
+	mkdir -p "${scratch}/trimming"
 	
 	samplelist="${work}/sample_ids.txt"
 	adapter_path="/sc/arion/work/arayan01/test-env/envs/atacseq/share/trimmomatic-0.39-2/adapters/NexteraPE-PE.fa"
@@ -40,10 +39,10 @@ function trimming {
         bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${scratch}/job_atactrimming_${sample}.txt" \
         "trimmomatic PE -threads 2 \
         ${R1} ${R2} \
-        ${scratch}/trimmed/${sample}_tr_1P.fastq.gz \
-        ${scratch}/trimmed/${sample}_tr_1U.fastq.gz \
-        ${scratch}/trimmed/${sample}_tr_2P.fastq.gz \
-        ${scratch}/trimmed/${sample}_tr_2U.fastq.gz \
+        ${scratch}/trimming/${sample}_tr_1P.fastq.gz \
+        ${scratch}/trimming/${sample}_tr_1U.fastq.gz \
+        ${scratch}/trimming/${sample}_tr_2P.fastq.gz \
+        ${scratch}/trimming/${sample}_tr_2U.fastq.gz \
         ILLUMINACLIP:${adapter_path}:2:30:10 MINLEN:87" #2:30:10 tell Trimmomatic to cut the adapters allowing a maximum of 2 mismatches, a score of 30 for paired reads, and score of 10 for single reads 
 														#MINLEN option tells Trimmomatic to drop reads shorter than 30 base pairs.
 	done < "${samplelist}"
@@ -55,22 +54,24 @@ function fastqc_trimming {
 
 	samplelist="${work}/sample_ids.txt"
 	qc_output="${work}/qc/trimming"
-	trimmed_dir="${scratch}/trimming"
+	trimming_dir="${scratch}/trimming"
 
 	while IFS=$'\t' read -r sample R1 R2; do
-		fastqc -t 2 "${trimmed_dir}/${sample}_tr_1P.fastq.gz" "${trimmed_dir}/${sample}_tr_2P.fastq.gz" -o "${qc_output}"
+		fastqc -t 2 "${trimming_dir}/${sample}_tr_1P.fastq.gz" "${trimming_dir}/${sample}_tr_2P.fastq.gz" -o "${qc_output}"
 	done < "${samplelist}"
 
 }
 
 function indexing {
 	gen_dir="${scratch}/refgenome"
-	bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${scratch}/job_atacgenomeindexing.txt" \
+	bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${work}/job_atacgenomeindexing.txt" \
 	"bwa index -p ${gen_dir}/hg38  ${gen_dir}/hg38.fasta"
 
 }
 
 function mapping {
+    rm -rf ${scratch}/mapping
+    mkdir -p ${scratch}/mapping
 	ref_gen="${scratch}/refgenome/hg38"
 	mapped_dir="${scratch}/mapping"
 	sample_dir="${scratch}/trimming"
@@ -81,7 +82,7 @@ function mapping {
         sample2="${sample}_tr_2P.fastq.gz"
 
         # Submit job to bsub
-        bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${scratch}/job_atacmapping_${basename}.txt" \
+        bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${work}/job_atacmapping_${basename}.txt" \
         "bwa mem -t 2 ${ref_gen} ${sample_dir}/${sample1} ${sample_dir}/${sample2} \
         | samtools sort -@2 -o ${mapped_dir}/${basename}.bam - \
         && samtools index ${mapped_dir}/${basename}.bam \
@@ -102,7 +103,7 @@ function pre_peakcalling_processing {
             "${mapped_dir}/${basename}_markdup_metrics.txt" \
             "${mapped_dir}/${basename}_SC_subset_dedup_map_stats.txt" \
             "job2_prepeakcalling_${basename}.txt"
-        bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "job2_prepeakcalling_${basename}.txt" \
+        bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${work}/prepeakcalling_${basename}.txt" \
             "samtools view -bh ${mapped_dir}/${basename}.bam > ${mapped_dir}/${basename}_SC_subset.bam && \
             picard MarkDuplicates I=${mapped_dir}/${basename}_SC_subset.bam O=${mapped_dir}/${basename}_SC_subset_dedup.bam M=${mapped_dir}/${basename}_markdup_metrics.txt && \
             samtools index ${mapped_dir}/${basename}_SC_subset_dedup.bam && \
@@ -119,18 +120,16 @@ function peakcalling {
 
     while IFS=$'\t' read -r sample R1 R2; do
         basename="${sample}"
-        bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "job3_${basename}.txt" \
+        bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${work}/peakcalling_${basename}.txt" \
             "macs2 callpeak -t ${mapped_dir}/${basename}_SC_subset_dedup.bam -f BAMPE -n ${peakcall}/${basename}_peak -g hs --keep-dup all"
     done < "${samplelist}"
 }
 
 #samplelist
 #fastqc_initial
+trimming
 #trimming
-#trimming2
-#fastqc_trimming
-#refgen_indexing
-#mapping1
-#mapping2
+#indexing
+#mapping
 #pre_peakcalling_processing
 #peakcalling
