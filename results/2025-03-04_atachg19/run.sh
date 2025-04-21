d022d69985bd74300a906fc1b736c071879b6897
nicolasarayaventura <nicolasaraya1825@gmail.com>
2025-04-21 12:31:26 -0400

    update

--- results/2025-03-04_atachg19/run.sh
+++ results/2025-03-04_atachg19/run.sh
@@ -4,84 +4,29 @@ adapter_path="/sc/arion/work/arayan01/test-env/envs/atacseq/share/trimmomatic-0.
 scratch="/sc/arion/scratch/arayan01/projects/personalized_epigenome/data/2025-03-04_atachg19"
 work="/sc/arion/work/arayan01/project/personalized_epigenome/results/2025-03-04_atachg19"
 gen_dir="/sc/arion/scratch/arayan01/projects/personalized_epigenome/data/hg19_rfgen"
-
-function samplelist {
-	rm -rf "${work}/sample_ids.txt"
-	sample_dir="/sc/arion/scratch/arayan01/projects/personalized_epigenome/data/2025-02-19_ranjan_data/2022-11-22_data_from_Ranjan_Xiang/atac"
-	output="${work}/sample_ids.txt"
-
-	samplepath=$(realpath "${sample_dir}")
-    find "${sample_dir}" -name "*_R1_001.fastq.gz" | while read R1; do
-        sample=$(basename "${R1}" | sed 's/_R1_001.fastq.gz//')
-        R2="${R1/_R1_001.fastq.gz/_R2_001.fastq.gz}"
-        echo -e "${sample}\t${R1}\t${R2}" >> "${output}"
-    done
-
-}
-
-function fastqc_initial {
-	samplelist="${work}/sample_ids.txt"
-	qc_out="${work}/qc/init_fastqc" #once job is finished run multiqc in directory!
-	sample_dir="/sc/arion/scratch/arayan01/projects/personalized_epigenome/data/2025-02-19_ranjan_data/2022-11-22_data_from_Ranjan_Xiang/atac"
-	while IFS=$'\t' read -r sample fastq_file; do
-		bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${scratch}/job_atacfastqc_${sample}.txt" \
-		fastqc -t 2 "${sample_dir}/${fastq_file}" -o "${qc_out}"
-	done < "${samplelist}"
-}
-
-function trimming {
-	rm -rf "${scratch}/trimming"
-	mkdir -p "${scratch}/trimming"
-	
-	samplelist="${work}/sample_ids.txt"
-	adapter_path="/sc/arion/work/arayan01/test-env/envs/atacseq/share/trimmomatic-0.39-2/adapters/NexteraPE-PE.fa"
-
-	while IFS=$'\t' read -r sample R1 R2; do
-        bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${work}/job_atactrimming_${sample}.txt" \
-        "trimmomatic PE -threads 2 \
-        ${R1} ${R2} \
-        ${scratch}/trimming/${sample}_tr_1P.fastq.gz \
-        ${scratch}/trimming/${sample}_tr_1U.fastq.gz \
-        ${scratch}/trimming/${sample}_tr_2P.fastq.gz \
-        ${scratch}/trimming/${sample}_tr_2U.fastq.gz \
-        ILLUMINACLIP:${adapter_path}:2:30:10 MINLEN:87" #2:30:10 tell Trimmomatic to cut the adapters allowing a maximum of 2 mismatches, a score of 30 for paired reads, and score of 10 for single reads 
-														#MINLEN option tells Trimmomatic to drop reads shorter than 30 base pairs.
-	done < "${samplelist}"
-}
-
-function fastqc_trimming {
-	rm -rf "${work}/qc/trimming"
-	mkdir -p "${work}/qc/trimming"
-
-	samplelist="${work}/sample_ids.txt"
-	qc_output="${work}/qc/trimming"
-	trimming_dir="${scratch}/trimming"
-
-	while IFS=$'\t' read -r sample R1 R2; do
-		fastqc -t 2 "${trimming_dir}/${sample}_tr_1P.fastq.gz" "${trimming_dir}/${sample}_tr_2P.fastq.gz" -o "${qc_output}"
-	done < "${samplelist}"
-
-}
+samplelist="/sc/arion/work/arayan01/project/personalized_epigenome/results/2025-02-24_atachg38/sample_ids.txt"
 
 function filteredref {
-    grep -vE "Un|random|alt|M|_" ${gen_dir}/hg19.fa.fai > ${gen_dir}/hg19_filtered.fa.fai
-    cut -f1 ${gen_dir}/hg19_filtered.fa.fai > ${gen_dir}/filtered_chromosomes.txt
+    samtools faidx "${gen_dir}/hg19.fa"
+    grep -vE "Un|random|alt" "${gen_dir}/hg19.fa.fai" > "${gen_dir}/hg19_filtered.fa.fai"
+    cut -f1 "${gen_dir}/hg19_filtered.fa.fai" > "${gen_dir}/filtered_chromosomes.txt"
 
-    while read chr; do samtools faidx ${gen_dir}/hg19.fa $chr >> ${gen_dir}/hg19_filtered.fa; done < ${gen_dir}/filtered_chromosomes.txt
+    rm -f "${gen_dir}/hg19_filtered.fa"
+    while read chr; do
+        samtools faidx "${gen_dir}/hg19.fa" "$chr" >> "${gen_dir}/hg19_filtered.fa"
+    done < "${gen_dir}/filtered_chromosomes.txt"
 
-
-	bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${work}/job_hg19filterd_indexing.txt" \
-	"bwa index -p ${gen_dir}/hg19_filtered ${gen_dir}/hg19_filtered.fa"
-
+    bsub -P acc_oscarlr -q premium -W 24:00 -R "rusage[mem=16000]" \
+    -o "${work}/job_hg19filterd_indexing.txt" -eo "${work}/job_hg19filterd_indexing_err.txt" \
+    "bwa index -p ${gen_dir}/hg19_filtered ${gen_dir}/hg19_filtered.fa"
 }
 
 function mapping {
     rm -rf ${scratch}/mapping
     mkdir -p ${scratch}/mapping
-	ref_gen="${scratch}/refgenome/hg19"
+	ref_gen="${gen_dir}/hg19_filtered"
 	mapped_dir="${scratch}/mapping"
-	sample_dir="${scratch}/trimmed"
-    samplelist="${work}/sample_ids.txt"
+	sample_dir="/sc/arion/scratch/arayan01/projects/personalized_epigenome/data/2025-02-24_atachg38/trimming"
 
 	while IFS=$'\t' read -r sample R1 R2; do
         basename="${sample}"
@@ -89,15 +34,14 @@ function mapping {
         sample2="${sample}_tr_2P.fastq.gz"
 
         # Submit job to bsub
-        bsub -P acc_oscarlr -q premium -n 2 -W 24:00 -R "rusage[mem=8000]" -o "${work}/job_atacmapping_${basename}.txt" \
-        "bwa mem -t 2 ${ref_gen} ${sample_dir}/${sample1} ${sample_dir}/${sample2} \
-        | samtools sort -@2 -o ${mapped_dir}/${basename}.bam - \
+        bsub -P acc_oscarlr -q premium -n 4 -W 24:00 -R "rusage[mem=8000]" -o "${work}/job_atacmapping_${basename}.txt" \
+        "bwa mem -t 4 ${ref_gen} ${sample_dir}/${sample1} ${sample_dir}/${sample2} \
+        | samtools sort -@4 -o ${mapped_dir}/${basename}.bam - \
         && samtools index ${mapped_dir}/${basename}.bam \
         && samtools flagstat ${mapped_dir}/${basename}.bam > ${mapped_dir}/${basename}_map_stats.txt"
     done < "${samplelist}"
 }
 function pre_peakcalling_processing {
-    samplelist="${work}/sample_ids.txt"
     mapped_dir="${scratch}/mapping"
 
     while IFS=$'\t' read -r sample R1 R2; do
@@ -118,7 +62,6 @@ function pre_peakcalling_processing {
 }
 
 function peakcalling {
-    samplelist="${work}/sample_ids.txt"
     mapped_dir="${scratch}/mapping"
     rm -rf "${work}/peakcalling"
     mkdir -p "${work}/peakcalling"
@@ -131,11 +74,7 @@ function peakcalling {
     done < "${samplelist}"
 }
 
-#samplelist
-#fastqc_initial
-#trimming
-#fastqc_trimming
-filteredref
-#mapping
+#filteredref
+mapping
 #pre_peakcalling_processing
 #peakcalling
